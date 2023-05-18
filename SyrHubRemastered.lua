9596411776fdda842ad2f89cc33064d0ed64349f
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-05-18 19:56:11 +0300

    Update SyrHubRemastered.lua

--- SyrHubRemastered.lua
+++ SyrHubRemastered.lua
@@ -6,6 +6,7 @@ local scriptswin = DiscordLib:Window("SyrHub: Remastered")
 
 -- servers
 local mainserv = scriptswin:Server("Syr Scripts", "cool")
+local secserv = scriptswin:Server("Tool Scripts & Misc", "")
 
 -- channels
 local slbtns = mainserv:Channel("Shindo Life")
@@ -29,6 +30,9 @@ local evadebtns = mainserv:Channel("Evade")
 local doorsbtns = mainserv:Channel("Doors")
 local ambtns = mainserv:Channel("Adopt Me")
 local arsbtns = mainserv:Channel("Arsenal")
+
+--channels for tools
+local miscbtns = mainserv:Channel("Misc Scripts")
 
 --scripts
 bfbtns:Button("Mukuro Hub BF", function()
@@ -122,4 +126,36 @@ end)
 
 arsbtns:Button("Arsenal OP GUI", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/HonestlyDex/DexHub/main/Init"))()
+end)
+
+
+--tools
+miscbtns:Button("Mobile Keyboard", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+end)
+
+miscbtns:Button("Fly GUI Mobile", function()
+loadstring("\108\111\97\100\115\116\114\105\110\103\40\103\97\109\101\58\72\116\116\112\71\101\116\40\40\39\104\116\116\112\115\58\47\47\103\105\115\116\46\103\105\116\104\117\98\117\115\101\114\99\111\110\116\101\110\116\46\99\111\109\47\109\101\111\122\111\110\101\89\84\47\98\102\48\51\55\100\102\102\57\102\48\97\55\48\48\49\55\51\48\52\100\100\100\54\55\102\100\99\100\51\55\48\47\114\97\119\47\101\49\52\101\55\52\102\52\50\53\98\48\54\48\100\102\53\50\51\51\52\51\99\102\51\48\98\55\56\55\48\55\52\101\98\51\99\53\100\50\47\97\114\99\101\117\115\37\50\53\50\48\120\37\50\53\50\48\102\108\121\37\50\53\50\48\50\37\50\53\50\48\111\98\102\108\117\99\97\116\111\114\39\41\44\116\114\117\101\41\41\40\41\10\10")()
+end)
+
+miscbtns:Button("Anti AFK Script", function()
+local vu = game:GetService("VirtualUser")
+game:GetService("Players").LocalPlayer.Idled:connect(function()
+   vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+   wait(1)
+   vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
+end)
+end)
+
+miscbtns:Button("Infinite Yield Script", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+end)
+
+miscbtns:Button("Infinite Jump", function()
+local InfiniteJumpEnabled = true
+game:GetService("UserInputService").JumpRequest:connect(function()
+	if InfiniteJumpEnabled then
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+	end
+end)
 end)
