81e88bdd5a7b6a89773d29be376f93bd5fb32e2e
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-06-04 09:52:19 +0300

    Update SyrHubRemastered.lua

--- SyrHubRemastered.lua
+++ SyrHubRemastered.lua
@@ -5,7 +5,7 @@ local DiscordLib = loadstring(game:HttpGet"https://raw.githubusercontent.com/daw
 local scriptswin = DiscordLib:Window("SyrHub: Remastered v1.1.7b")
 
 -- servers
-local mainserv = scriptswin:Server("Syr Scripts", "https://www.roblox.com/catalog/13646501116/SyrHubLogo")
+local mainserv = scriptswin:Server("Syr Scripts", "https://www.roblox.com/library/13646501082/SyrHubLogo")
 local secserv = scriptswin:Server("Tool Scripts & Misc", "")
 
 -- channels
