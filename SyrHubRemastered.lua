daa2c19c54ea702f0fb2a6ccb9282f2e14e37fec
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-06-08 11:07:13 +0300

    Update SyrHubRemastered.lua

--- SyrHubRemastered.lua
+++ SyrHubRemastered.lua
@@ -404,58 +404,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/AliCode14/scripts/mai
 end)
 
 ntbtns:Button("Quiz Solver", function()
---BROUGHT TO YOU BY RSCRIPTS.NET--
-
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/preztel/AzureLibrary/master/uilib.lua", true))()
-
-local Main = Library:CreateTab("Ninja Simulator", "Functions", false)
-
-Main:CreateToggle()
-   "Auto Quiz Solver",
-   function(kdss)
-       _G.toggles1dss = kdss or false
-       while _G.toggles1dss and wait(2) do
-           for i, v in pairs(game:GetService("Workspace")["TimedEvent_Quiz"]:GetDescendants()) do
-               if string.find(v.Name, "AnswerTarget") and v:FindFirstChild("OnDied") then
-                   game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
-                       v.HumanoidRootPart.CFrame + v.HumanoidRootPart.CFrame.lookVector * -1
-               end
-           end
-           wait(0.4)
-           plr = game.Players.LocalPlayer.Character
-           game:GetService("Workspace")[plr.Name].Combat.Attack:FireServer()
-           local N = game:GetService("VirtualInputManager")
-           N:SendKeyEvent(true, "F", false, game)
-       end
-   end
-)
-Main:CreateToggle(
-   "Auto Click",
-   function(kd55fs)
-       _G.toggle5fs21 = kd55fs or false
-       game:GetService("RunService").Stepped:connect(
-           function()
-               if _G.toggle5fs21 then
-                   fireclickdetector(
-                       game.workspace["Zednov's Tycoon Kit"].Tycoons[game.Players.LocalPlayer.Team.Name].PurchasedObjects.Mine.Clicker.ClickDetector
-                   )
-               end
-           end
-       )
-   end
-)
-Main:CreateButton(
-   "Made By: Bebo Mods",
-   function()
-       setclipboard("https://discord.com/invite/sdUSPyY65T")
-   end
-)
-Main:CreateButton(
-   "Join My Discord",
-   function()
-       setclipboard("https://discord.com/invite/sdUSPyY65T")
-   end
-)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CorruptedClan/quizSolver/main/NinjaTycoon.lua"))()
 end)
 
 ntbtns:Button("Black GUI 2", function()
