565d01a058ab5efbc4b728c98c39d0424f1d146a
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-07-11 22:50:59 +0300

    Update SyrHubRemastered.lua

--- SyrHubRemastered.lua
+++ SyrHubRemastered.lua
@@ -538,7 +538,7 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/1f0yt/community/main/
 end)
 
 ddbtns:Button("ZenX Beta (Paid)", function()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/CorruptedClan/dbdb/main/MainLoader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CorruptedClan/BestOutThere/main/ZenX.lua"))()
 end)
 
 mvsdbtns:Button("Black Hub", function()
